954e953d7ed8c343eb3dc7539080215cbd2aac8c
Hyan Mandian <hyanmandian@hotmail.com>
2025-01-26 03:37:32 -0300

    feat: add docs to cities data

--- src/_internals/cities.ts
+++ src/_internals/cities.ts
@@ -1,3 +1,26 @@
+/**
+ * A collection of Brazilian cities categorized by their respective states.
+ *
+ * @constant
+ * @type {Object}
+ * @property {string[]} GO - Cities in the state of Goiás.
+ * @property {string[]} MG - Cities in the state of Minas Gerais.
+ * @property {string[]} PA - Cities in the state of Pará.
+ * @property {string[]} CE - Cities in the state of Ceará.
+ * @property {string[]} BA - Cities in the state of Bahia.
+ * @property {string[]} PR - Cities in the state of Paraná.
+ * @property {string[]} SC - Cities in the state of Santa Catarina.
+ * @property {string[]} PE - Cities in the state of Pernambuco.
+ * @property {string[]} TO - Cities in the state of Tocantins.
+ * @property {string[]} RN - Cities in the state of Rio Grande do Norte.
+ * @property {string[]} PI - Cities in the state of Piauí.
+ * @property {string[]} RS - Cities in the state of Rio Grande do Sul.
+ * @property {string[]} MT - Cities in the state of Mato Grosso.
+ * @property {string[]} AC - Cities in the state of Acre.
+ * @property {string[]} SP - Cities in the state of São Paulo.
+ * @property {string[]} ES - Cities in the state of Espírito Santo.
+ * @property {string[]} MA - Cities in the state of Maranhão.
+ */
 export const DATA = {
 	GO: [
 		"Abadia de Goiás",
