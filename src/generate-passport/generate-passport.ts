da4e35665afd81cf8cd6ad7cae0ee1a40a322b38
Vicente Vendramin <guesser.vicente@gmail.com>
2026-04-07 11:14:20 -0300

    fix: lint

--- src/generate-passport/generate-passport.ts
+++ src/generate-passport/generate-passport.ts
@@ -1,10 +1,5 @@
 import { generateRandomNumber } from "../_internals/generate-random-number/generate-random-number";
-import {
-	ALPHABET_LENGTH,
-	CHAR_CODE_A,
-	DIGITS_LENGTH,
-	LETTERS_LENGTH,
-} from "./constants";
+import { ALPHABET_LENGTH, CHAR_CODE_A, DIGITS_LENGTH, LETTERS_LENGTH } from "./constants";
 
 /**
  * Generates a random valid Brazilian passport number.
@@ -17,9 +12,7 @@ import {
  */
 export const generatePassport = (): string => {
 	const letters = Array.from({ length: LETTERS_LENGTH }, () =>
-		String.fromCharCode(
-			CHAR_CODE_A + Math.floor(Math.random() * ALPHABET_LENGTH),
-		),
+		String.fromCharCode(CHAR_CODE_A + Math.floor(Math.random() * ALPHABET_LENGTH)),
 	).join("");
 
 	const digits = generateRandomNumber(DIGITS_LENGTH);
